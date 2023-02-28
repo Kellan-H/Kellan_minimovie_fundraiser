6bdf0492ddb36f862e1a5f6fbf25a451f9053812
Kellan-H <124210369+Kellan-H@users.noreply.github.com>
2023-02-28 14:41:46 +1300

    Add files via upload

---
+++ 01yes_no_v1.py
@@ -0,0 +1,12 @@
+want_instructions = input("Do you want to read the instructions? ")
+valid_input = False
+while valid_input == False:
+    if want_instructions == "yes":
+        print("instructions go here")
+        valid_input = True
+    elif want_instructions == "no":
+        valid_input = True
+        pass
+    else:
+        print("please enter yes or no")
+        valid_input = False
